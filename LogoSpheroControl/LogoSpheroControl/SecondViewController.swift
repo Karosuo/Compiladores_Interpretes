f8203daa48b8ef344f3df86bad2ef34692167d70
Rafael Karosuo <ic@ics-Mac-mini-6.local>
2016-12-22 08:31:42 -0800

    Command list showing correctly

--- LogoSpheroControl/LogoSpheroControl/SecondViewController.swift
+++ LogoSpheroControl/LogoSpheroControl/SecondViewController.swift
@@ -8,8 +8,36 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, UITableViewDataSource {
 
+    let commands = [
+        ("GO", "GO"),
+        ("FORWARD", "FORWARD"),
+        ("BACKWARD", "BACKWARD"),
+        ("LEFT", "LEFT"),
+        ("RIGHT","RIGHT"),
+        ("REPEAT","REPEAT N []"),
+        ("PRINT", "PRINT []"),
+        ("IF", "IF condition []"),
+        ("IF-ELSE", "IF condition [] ~ []"),
+        ("WORD", "WORD"),
+        ("PENUP", "PENUP"),
+        ("CLEAR SCREEN", "CLEARSCREEN"),
+        ("REPCOUNT", "REPCOUNT"),
+        ("SINE", "SIN N"),
+        ("COLOR", "COLOR N"),
+        ("NOT", "NOT"),
+        ("STEPS", "STEPS"),
+        ("START", "START"),
+        ("PENDOWN","PENDOWN"),
+        ("COSINE","COS N"),
+        ("NEW","NEW"),
+        ("END","END"),
+        ("HOME", "HOME"),
+        ("BYE", "BYE"),
+        ("MAKE","MAKE")
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +47,21 @@ class SecondViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        var (commandLabel, commandText) = commands[indexPath.row]
+        cell.textLabel?.text = commandLabel
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return commands.count
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
 
 }
 
